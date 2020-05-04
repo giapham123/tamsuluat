b4f7adcc668ac6e6467a580b809ca4b485952f0d
giapham <gia.pham@dounets.com>
2020-05-04 11:14:05 +0700

    qqqq

--- back-end/ecosystem.config.js
+++ back-end/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
       name: 'API',
-      script: 'server.js',
+      script: 'set NODE_ENV=development&&nodemon server.js',
   
       // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
       args: 'one two',
