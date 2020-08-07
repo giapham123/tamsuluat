393372ec05eb1d8052e52ec8c7f8260dc4b01628
morosdanic <dan@berlin-byt.es>
2020-08-07 22:27:05 +0700

    aa

--- front-end/src/views/home/index.vue
+++ front-end/src/views/home/index.vue
@@ -173,8 +173,8 @@ export default {
       for (let i = 0; i < resultCompany.length; i++) {
         // resultCompany[i]._source.image =
         //   "data:image/jpeg;base64," + resultCompany[i]._source.image;
-        this.itemsCompanyList.push(resultCompany[i]._source);
          resultCompany[i].image = process.env.VUE_APP_SERVER + resultCompany[i]._source.image;
+        this.itemsCompanyList.push(resultCompany[i]._source);
       }
       if (resultCompany.length > 0) {
         this.overlay = false;
